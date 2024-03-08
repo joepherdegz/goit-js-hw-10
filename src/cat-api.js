f6f3cd940fca99bb4c0a961f4c9e9384b842573f
joepherdegz <142654533+joepherdegz@users.noreply.github.com>
2024-03-08 10:18:45 -0800

    update

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 axios.defaults.headers.common["x-api-key"] = "live_NGPLX2y6hrCGJMaXLsUNBCQbsHykrFZRTPlCWPfte6OpU1KNrXMOhkr5ds9bwWJn"
 
-const BASE_URL = "https://api.thecatapi.com/v1";
-const API_KEY = "live_NGPLX2y6hrCGJMaXLsUNBCQbsHykrFZRTPlCWPfte6OpU1KNrXMOhkr5ds9bwWJn"
+export const BASE_URL = "https://api.thecatapi.com/v1";
+export const API_KEY = "live_NGPLX2y6hrCGJMaXLsUNBCQbsHykrFZRTPlCWPfte6OpU1KNrXMOhkr5ds9bwWJn"
 
 export const fetchBreeds = () => {
     return axios.get(`${BASE_URL}/breeds?api-key=${API_KEY}`)
@@ -19,7 +19,7 @@ export const fetchBreeds = () => {
 };
 
 export const fetchCatByBreed = (breedId) => {
-    return axios.get(`${BASE_URL}/imagessearch?api_key=${API_KEY}&breed_ids=${breedId}`)
+    return axios.get(`${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`)
         .then(response => {
             if (response.status >= 400) {
                 throw new Error(response.status);
@@ -27,7 +27,7 @@ export const fetchCatByBreed = (breedId) => {
                 return response.data;
             }
         })
-        catch (error => {
+        .catch (error => {
     throw new Error(error.response.status);
 });
 }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,56 @@
-// api key live_NGPLX2y6hrCGJMaXLsUNBCQbsHykrFZRTPlCWPfte6OpU1KNrXMOhkr5ds9bwWJn
+import axios from 'axios';
+import { fetchBreeds, fetchCatByBreed, } from "./cat-api";
+import { BASE_URL } from "./cat-api"
+import { API_KEY } from "./cat-api"
 
+const breedSelectEl = document.querySelector(".breed-select");
+const catInfoEl = document.querySelector(".cat-info");
+const loaderEl = document.querySelector(".loader");
+const errorEl = document.querySelector(".error");
+
+errorEl.classList.add("is-hidden");
+
+function chooseBreed() {
+    axios.get(`${BASE_URL}/breeds`)
+        .then((response) => {
+            const data = response.data;
+            loaderEl.classList.replace("loader", "is-hidden");
+
+            let optionsMarkup = data.map(({ name, id }) => {
+                return `<option value=${id}>${name}</option>`;
+            });
+            breedSelectEl.insertAdjacentHTML("beforeend", optionsMarkup);
+            breedSelectEl.classList.remove("is-hidden");
+        })
+        .catch(onError);
+}
+
+chooseBreed();
+
+breedSelectEl.addEventListener("change", (e) => {
+    loaderEl.classList.replace("is-hidden", "loader");
+    catInfoEl.classList.add("is-hidden");
+
+    let breedId = e.target.value;
+    axios.get(`${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`)
+        .then((response) => {
+            const data = response.data;
+            const { url, breeds } = data[0];
+            const { name, description, temperament } = breeds[0];
+
+            catInfoEl.innerHTML = `
+            <img src='${url}' alt='{name}' width="400"/>
+            <div class='box'>
+                <h2>${name}</h2>
+                <p>${description}</p>
+                <p><b>Temperament:</b> ${temperament}</p>
+            </div> `;
+            catInfoEl.classList.remove("is-hidden");
+            loaderEl.classList.add("is-hidden");
+        })
+        .catch(onError);
+})
+function onError() {
+    errorEl.classList.remove("is-hidden");
+    breedSelectEl.classList.add("is-hidden")
+}
